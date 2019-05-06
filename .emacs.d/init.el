dfa8cadd9c012bfe75812901c023fcba22832cf0
Denys Mentiei <endden@gmail.com>
2019-05-06 22:45:12 +0300

    emacs: Package contents refresh.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -140,6 +140,9 @@
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
 
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
