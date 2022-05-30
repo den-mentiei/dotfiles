f6f2971c117a7d201880ae5746ed597943056dc3
Denys Mentiei <endden@gmail.com>
2022-05-31 01:22:27 +0300

    emacs: Massive rehaul.

---
+++ .emacs.d/early-init.el
@@ -0,0 +1,130 @@
+;; -*- lexical-binding: t; -*-
+
+;; Increase the GC threshold for faster startup.
+;; Measured in bytes.
+(setq gc-cons-threshold (* 10 1024 1024))
+;; (setq gc-cons-threshold most-positive-fixnum)
+
+;; Done to redirect custom generated code, not using it anyway.
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file 'noerror)
+
+;; In noninteractive sessions, prioritize non-byte-compiled source
+;; files to prevent the use of stale byte-code. Otherwise, it saves us
+;; a little IO time to skip the mtime checks on every *.elc file.
+(setq load-prefer-newer noninteractive)
+
+;; Native compilation settings
+(when (featurep 'native-compile)
+  ;; Silence compiler warnings as they can be pretty annoying.
+  (setq native-comp-async-report-warnings-errors 'silent)
+  ;; Let's compile asynchronously.
+  (setq native-comp-deferred-compilation t))
+
+(unless (or (daemonp)
+			noninteractive
+			init-file-debug)
+  ;; Premature redisplays can substantially affect startup times and
+  ;; produce ugly flashes before theme is loaded.
+  (setq-default inhibit-redisplay t)
+  (setq-default inhibit-message t)
+  (add-hook 'window-setup-hook
+			(lambda ()
+			  (setq-default inhibit-redisplay nil)
+			  (setq-default inhibit-message nil)
+			  (redisplay)))
+  ;; Site files tend to use `load-file', which emits "Loading X..."
+  ;; messages in the echo area, which in turn triggers a
+  ;; redisplay. Redisplays can have a substantial effect on startup
+  ;; times and in this case happens so early that Emacs may flash
+  ;; white while starting up.
+  (define-advice load-file (:override (file) silence)
+	(load file nil 'nomessage))
+  ;; Undo the `load-file' advice above, to limit the scope of any edge
+  ;; cases it may introduce down the road.
+  (define-advice startup--load-user-init-file (:before (&rest _) init-doom)
+    (advice-remove #'load-file #'load-file@silence)))
+
+;;; Bootstrap
+
+;; Do not warn about defadvice redefinitions.
+(setq ad-redefinition-action 'accept)
+
+;; The only thing that is needed to make utf-8 the default coding
+;; system.
+(set-language-environment "UTF-8")
+;; `set-language-environment` sets `default-input-method`, which is
+;; not needed.
+(setq default-input-method nil)
+
+(defconst my/is-macos (eq system-type 'darwin))
+(defconst my/is-linux (eq system-type 'gnu-linux))
+(defconst my/is-win (memq system-type '(cygwin windows-nt msd-dos)))
+
+;; Disable bidirectional text scanning for a modest performance
+;; boost. I've set this to `nil' in the past, but the
+;; `bidi-display-reordering's docs say that is an undefined state and
+;; suggest this to be just as good:
+(setq-default bidi-display-reordering 'left-to-right)
+(setq-default bidi-paragraph-direction 'left-to-right)
+
+;; Disabling the BPA makes redisplay faster, but might produce
+;; incorrect display reordering of bidirectional text with embedded
+;; parentheses and other bracket characters whose 'paired-bracket'
+;; Unicode property is non-nil.
+(setq bidi-inhibit-bpa t)
+
+;; Reduce rendering/line scan work for Emacs by not rendering cursors
+;; or regions in non-focused windows.
+(setq-default cursor-in-non-selected-windows nil)
+(setq highlight-nonselected-windows nil)
+
+;; More performant rapid scrolling over unfontified regions. May cause
+;; brief spells of inaccurate syntax highlighting right after
+;; scrolling, which should quickly self-correct.
+(setq fast-but-imprecise-scrolling t)
+
+;; Don't ping things that look like domain names.
+(setq ffap-machine-p-known 'reject)
+
+;; Resizing the Emacs frame can be a terribly expensive part of
+;; changing the font. By inhibiting this, we halve startup times,
+;; particularly when we use fonts that are larger than the system
+;; default (which would resize the frame).
+(add-to-list 'frame-inhibit-implied-resize 'font)
+
+;; Font compacting can be terribly expensive, especially for rendering
+;; icon fonts on Windows. Whether disabling it has a notable affect on
+;; Linux and Mac hasn't been determined, but do it there anyway, just
+;; in case. This increases memory usage, however!
+(setq inhibit-compacting-font-caches t)
+
+;; No need to see the splash screen.
+(setq inhibit-startup-screen t)
+;; Hides yet another startup *message*.
+(setq inhibit-startup-echo-area-message user-login-name)
+;; No need to load any default.el.
+(setq inhibit-default-init t)
+;; Make the initial buffer load faster by setting it to fundamental
+;; mode.
+(setq initial-major-mode 'fundamental-mode)
+;; Empty your mind!
+(setq initial-scratch-message nil)
+
+;; Removing unused UI things.
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+
+;; Performance on Windows is considerably worse than elsewhere. We'll
+;; need everything we can get.
+(when (boundp 'w32-get-true-file-attributes)
+  ;; Decreases file IO workload.
+  (setq w32-get-true-file-attributes nil)
+  ;; Faster IPC.
+  (setq w32-pipe-read-delay 0)
+  ;; Read more at a time (was 4K).
+  (setq w32-pipe-buffer-size (* 64 1024))
+  ;; The clipboard on Windows could be in another encoding (likely
+  ;; utf-16), so let Emacs/the OS decide what to use there.
+  (setq selection-coding-system 'utf-8))
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,98 +1,15 @@
-;; -*- no-byte-compile: t; lexical-binding: t; -*-
+;; -*- lexical-binding: t; -*-
 
-(require 'cl)
+;; Profile startup time
+(add-hook 'emacs-startup-hook
+		  (lambda ()
+			(message "Emacs loaded in %s." (emacs-init-time))))
 
-;; Done to redirect custom generated code, not using it anyway.
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(load custom-file 'noerror)
-
-(setq comp-deferred-compilation t)
-(setq native-comp-async-report-warnings-errors 'silent)
-
-;;; Basic stuff
-
-(setq gc-cons-threshold (* 10 1024 1024))
-
+;; That's me.
 (setq user-full-name "Denys Mentiei")
 (setq user-mail-address "endden@gmail.com")
 
-(setq make-backup-files nil)
-(setq backup-by-copying t)
-(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
-(setq delete-old-versions t)
-(setq version-control t)
-(setq create-lockfiles nil)
-(setq auto-save-default nil)
-
-(define-coding-system-alias 'cp65001 'utf-8)
-(set-language-environment "UTF-8")
-(prefer-coding-system 'utf-8)
-(setq locale-coding-system 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
-(when (display-graphic-p)
-   (setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING)))
-
-; ask "y"/"n" instead of "yes"/"no"
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-(setq tags-revert-without-query 1)
-
-(setq initial-scratch-message "")
-(setq inhibit-splash-screen t)
-
-(setq frame-title-format "%f")
-
-(tooltip-mode -1)
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-
-(setq ring-bell-function 'ignore)
-
-(setq use-dialog-box nil)
-
-(setq help-window-select t)
-
-(setq-default show-paren-delay 0)
-(show-paren-mode t)
-
-(setq search-highlight t)
-
-(setq-default tab-width 4)
-(setq-default indent-tabs-mode t)
-(setq-default indent-line-function 'insert-tab)
-
-(setq mouse-wheel-scroll-amount '(1))
-(setq mouse-wheel-follow-mouse 't)
-(setq scroll-conservatively 101)
-(setq scroll-margin 10)
-
-(global-hl-line-mode t)
-(column-number-mode t)
-
-(global-superword-mode 1)
-
-;; Automatically update unmodified buffers whose files have changed.
-(global-auto-revert-mode 1)
-
-;; Enforces horizontal only splits.
-(setq split-height-threshold nil)
-(setq split-width-threshold 0)
-
-;; Do not warn about defadvice redefinitions.
-(setq ad-redefinition-action 'accept)
-
-(defadvice align-regexp (around align-regexp-with-spaces)
-  "Use spaces for aligning as opposed to tabs for indentation."
-  (let ((indent-tabs-mode nil))
-	ad-do-it))
-(ad-activate 'align-regexp)
-
-;; Do not resize the frame when font size is changed.
-(add-to-list 'frame-inhibit-implied-resize 'font)
+;;; General UI/UX
 
 ; 0123456789abcdefghijklmnopqrstuvwxyz [] () :;,. !@#$^&*
 ; 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ {} <> "'`  ~-_/|\?
@@ -107,25 +24,102 @@
 (when (member "Noto Color Emoji" (font-family-list))
   (set-fontset-font t 'symbol (font-spec :family "Noto Color Emoji") nil 'prepend))
 
-(require 'dired)
-(setq dired-recursive-deletes 'top)
+;; Filename is enough.
+(setq frame-title-format "%f - emacs")
+(setq icon-title-format frame-title-format)
 
-;; Trust all themes.
-(setq custom-safe-themes t)
+;; GUIs are inconsistent on different systems and Emacs can handle the
+;; prompting just fine.
+(setq use-dialog-box nil)
+(when (bound-and-true-p tooltip-mode)
+  (tooltip-mode -1))
+(when my/is-linux
+  (setq x-gtk-use-system-tooltips nil))
 
-;;; Functions
+;; Favor vertical splits over horizontal ones. Monitors are wide,
+;; dude!
+(setq split-width-threshold 0)
+(setq split-height-threshold nil)
 
-(defun my/win-p ()
-  "Check if a system is running windows."
-  (eq system-type 'windows-nt))
+;; Simpler buffer names for same base ones.
+(setq uniquify-buffer-name-style 'forward)
 
-(defun my/macos-p ()
-  "Check if a system is running macos."
-  (eq system-type 'darwin))
+;; No bells!
+(setq ring-bell-function #'ignore)
+(setq visual-bell nil)
 
-(defun my/linux-p ()
-  "Check if a system is running linux."
-  (eq system-type 'gnu/linux))
+;; Shows matching parens immediately.
+(setq-default show-paren-delay 0)
+(show-paren-mode t)
+
+;;; Lines/Columns
+
+;; Explicitly set to reduce the cost of on-the-fly computation.
+(setq-default display-line-numbers-width 3)
+
+;; Show absolute numbers for narrowed regions, not to get lost.
+(setq-default display-line-numbers-widen t)
+
+;; Highlights the current line.
+(global-hl-line-mode t)
+;; Shows column number in the modeline.
+(column-number-mode t)
+
+;;; Minibuffer
+
+;; Allow for minibuffer nesting.
+(setq enable-recursive-minibuffers t)
+
+;; Show current unfinished key-sequence to see what happens.
+(setq echo-keystrokes 0.02)
+
+;; Ask "y"/"n" instead of "yes"/"no".
+(setq use-short-answers t)
+
+;;; Editor.
+
+;; Disables the warning "X and Y are the same file". Emacs will
+;; redirect to thge existing buffer, anyway.
+(setq find-file-suppress-same-file-warnings t)
+
+;; Automatically update unmodified buffers whose files have changed.
+(global-auto-revert-mode 1)
+
+;; No backups, yolo.
+(setq make-backup-files nil)
+;; Some sensible defaults if backups are ever get enabled.
+(setq backup-by-copying t)
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+(setq delete-old-versions t)
+(setq version-control t)
+(setq create-lockfiles nil)
+(setq auto-save-default nil)
+
+;;; Formatting.
+
+;; Tabs > spaces. It can be changed on a per-mode basis (or even via
+;; .dir-locals.el) anyway.
+(setq-default tab-width 4)
+(setq-default indent-tabs-mode t)
+(setq-default indent-line-function 'insert-tab)
+
+(defadvice align-regexp (around align-regexp-with-spaces)
+  "Use spaces for aligning as opposed to tabs for indentation."
+  (let ((indent-tabs-mode nil))
+	ad-do-it))
+(ad-activate 'align-regexp)
+
+;; Only indent the line when at the beginning or in existing
+;; indentation.
+(setq-default tab-always-indent nil)
+
+;; All the horizontal splits take the screen real-estate!
+(setq-default fill-column 70)
+
+;; Archaic thing that is no longer a thing.
+(setq sentence-end-double-space nil)
+
+;;; Utilities.
 
 (defun my/find-user-init-file ()
   "Edits the `user-init-file` in another window."
@@ -136,11 +130,6 @@
   "Kills all open buffers."
   (interactive)
   (mapc 'kill-buffer (buffer-list)))
-
-(defun my/kill-current-buffer ()
-  "Kills the current buffer."
-  (interactive)
-  (kill-buffer (buffer-name)))
 
 (defun my/show-installed-packages ()
   "Shows installed non-builtin packages."
@@ -154,19 +143,15 @@
 		  (not (package-built-in-p x))))
 	  (mapcar 'car package-archive-contents))))
 
-(defun my/cc-settings ()
-  "Bunch of default settings valid for C-mode"
+(defun my/default-tab-settings ()
+  "Bunch of default settings valid for prog modes."
   (interactive)
   (setq tab-width 4)
   (setq c-basic-offset 4)
   (setq indent-tabs-mode t)
   (modify-syntax-entry ?_ "w"))
 
-;; Trying to make win performance better :(
-(when (and (my/win-p) (boundp 'w32-pipe-read-delay))
-  (setq w32-pipe-read-delay 0))
-
-;;; Packages
+;;; Packages.
 
 (require 'package)
 
@@ -178,18 +163,36 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
+;; Initialize use-package.
 (unless (package-installed-p 'use-package)
-  (package-refresh-contents)
   (package-install 'use-package))
 (eval-when-compile
   (require 'use-package))
 (setq use-package-always-ensure t)
 
-;;; Mode-line
+;;; Built-ins.
 
-; Do not forget to call all-the-icons-install-fonts.
+(use-package dired
+  :ensure nil
+  :commands (dired dired-jump)
+  :config
+  ;; Ask for any top folder to be on the safe side.
+  (setq dired-recursive-deletes 'top))
+
+;;; Etc.
+
+(use-package exec-path-from-shell
+  :if (memq window-system '(mac ns x))
+  :config
+  (exec-path-from-shell-initialize))
+
+;;; Mode-line.
+
+;; Icons for major-modes, etc.
+;; Do not forget to call `all-the-icons-install-fonts`.
 (use-package all-the-icons)
 
+;; I'm using Doom for now as it looks nice and is pretty lightweight.
 (use-package doom-modeline
   :config
   (setq doom-modeline-icon t)
@@ -208,6 +211,7 @@
   :hook
   (after-init . doom-modeline-mode))
 
+;; Solarized theme thing.
 (setq my-solarized-faces
 	  '("My solarized theme customization."
 		(custom-theme-set-faces
@@ -249,44 +253,58 @@
   (deftheme my-solarized-dark "The dark varian of the Solarized colour theme.")
   (solarized-with-color-variables 'dark 'my-solarized-dark solarized-dark-color-palette-alist my-solarized-faces))
 
-(use-package general)
-
-(use-package hydra
-  :config
-  ;;; TODO: Customize border/bg/fringes via
-  ;;; https://github.com/abo-abo/hydra/blob/master/hydra.el#L227
-  (setq hydra-hint-display-type 'posframe)
-  :init
-  (use-package use-package-hydra))
-
-(use-package evil
-  :config
-  (setq evil-auto-indent t)
-  (setq evil-echo-state nil)
-  (setq evil-want-Y-yank-to-eol t)
-  (evil-mode 1))
-
 (use-package diminish
   :config
   (diminish 'eldoc-mode)
   (diminish 'undo-tree-mode))
 
+;; Vim once, Vim forever, dude.
+(use-package evil
+  :init
+  ;; TODO(dmi): @incomplete Use `evil-collection`.
+  ;; (setq evil-want-integration t)
+  (setq evil-want-C-i-jump nil)
+  (setq evil-auto-indent t)
+  (setq evil-echo-state nil)
+  (setq evil-want-Y-yank-to-eol t)
+  :config
+  (evil-mode 1))
+
+(use-package general
+  :after evil)
+
 (use-package which-key
   :diminish 'which-key-mode
-  :config
+  :init
   (setq which-key-separator " ")
   (setq which-key-prefix-prefix "+")
+  :config
   (which-key-mode 1))
+
+(use-package magit
+  :config
+  ; Disables the automatic diff show-off of the changes about to commit.
+  (remove-hook 'server-switch-hook 'magit-commit-diff)
+  (add-to-list 'magit-no-confirm 'stage-all-changes)
+  (setq vc-handled-backends '(git))
+  (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
+  (evil-set-initial-state 'magit-log-edit-mode 'insert)
+  (add-hook 'git-commit-mode-hook 'evil-insert-state)
+  :general
+  ("C-x g" 'magit-status))
 
 (use-package org
   :mode ("\\.org$" . org-mode)
+  :commands (org-capture org-agenda)
   :diminish 'org-indent-mode
   :general
   ("C-c c" 'org-capture)
   :config
+  ;; A bit nicer than the default `...`.
+  (setq org-ellipsis " ▾")
   (setq org-default-notes-file (concat org-directory "/inbox.org"))
   (setq org-src-fontify-natively t)
-  (setq org-M-RET-may-split-line '((item . nil)))
+  (setq org-M-RET-may-split-line nil)
   (setq org-hide-leading-stars t)
   (setq org-startup-indented t)
   (setq org-indent-indentation-per-level 1)
@@ -306,6 +324,144 @@
   (setq org-hide-emphasis-markers t)
   ;;; Time will be recorded after task completion.
   (setq org-log-done 'time))
+
+;; Typography things.
+(use-package typo
+  :diminish 'typo-mode
+  :hook
+  (org-mode . typo-mode))
+
+;;; Code completion.
+
+(use-package company
+  :diminish 'company-mode
+  :custom
+  (company-dabbrev-downcase nil)
+  (company-idle-delay 0.1)
+  :config
+  (global-company-mode))
+
+(use-package eglot)
+
+;;; File format modes.
+
+;; Rust.
+(use-package rust-mode
+  :mode ("\\.rs\\'" . rust-mode)
+  :hook
+  (rust-mode . my/default-tab-settings))
+
+;; Haskell.
+(use-package haskell-mode
+  :mode ("\\.hs\\'" "\\.lhs\\'" "\\.hsc\\'" "\\.cpphs\\'" "\\.c2hs\\'")
+  :config
+  (setq haskell-compile-cabal-build-command "stack build")
+  :config
+  (defun my/haskell-settings ()
+	"Bunch of default settings valid for haskell-mode"
+	(interactive)
+	(setq tab-width 2)
+	(setq c-basic-offset 2)
+	(setq indent-tabs-mode nil))
+  :hook
+  (haskell . my/haskell-settings))
+
+(use-package csharp-mode
+  :mode ("\\.cs\\'"))
+
+(use-package typescript-mode
+  :mode ("\\.ts\\'"))
+
+(use-package swift-mode
+  :mode ("\\.swift\\'"))
+
+(use-package kotlin-mode
+  :mode ("\\.kt\\'"))
+
+
+(use-package anaconda-mode
+  :mode ("\\.py$" . python-mode)
+  :diminish anaconda-mode
+  :config
+  (defun my/python-settings ()
+	(setq tab-width 4)
+	(setq indent-tabs-mode t)
+	(setq indent-line-function 'insert-tab))
+  :hook
+  ((python-mode . anaconda-mode)
+   (python-mode . my/python-settings)))
+
+(use-package company-anaconda
+  :after (company anaconda-mode)
+  :config
+  (add-to-list 'company-backends 'company-anaconda))
+
+(use-package elm-mode
+  :mode ("\\.elm\\'" . elm-mode)
+  :after company
+  :config
+  (add-to-list 'company-backends 'company-elm))
+
+(use-package solidity-mode
+  :mode ("\\.sol\\'")
+  :config
+  (setq solidity-comment-style 'slash))
+(use-package company-solidity
+  :after (company solidity-mode)
+  :config
+  (add-to-list 'company-backends 'company-solidity))
+
+;; Oh, god.
+(use-package php-mode
+  :mode ("\\.php\\'"))
+
+(use-package web-mode
+  :mode ("\\.html\\'" "\\.css\\'" "\\.tsx\\'"))
+
+(use-package markdown-mode
+  :mode ("\\.md\\'"))
+
+(use-package dockerfile-mode
+  :mode ("Dockerfile\\'"))
+
+(use-package yaml-mode
+  :mode ("\\.yml\\'". yaml-mode))
+
+;;; Bindings.
+
+(defconst my/leader "SPC")
+(general-create-definer my/leader-def :prefix my/leader)
+
+(general-define-key
+  :states 'normal
+  "C-h"     'windmove-left
+  "C-j"     'windmove-down
+  "C-k"     'windmove-up
+  "C-l"     'windmove-right)
+
+(general-define-key
+ :states 'insert
+ "C-v" 'evil-paste-before)
+
+(my/leader-def
+  :states 'normal
+  "i"   'imenu
+  "e i" 'my/find-user-init-file
+  "w"   'writeroom-mode)
+
+(my/leader-def
+  :states 'visual
+  "c" 'comment-or-uncomment-region)
+
+;; OLD =============================
+
+(use-package hydra
+  :config
+  ;;; TODO: Customize border/bg/fringes via
+  ;;; https://github.com/abo-abo/hydra/blob/master/hydra.el#L227
+  (setq hydra-hint-display-type 'posframe)
+  :init
+  (use-package use-package-hydra))
 
 (use-package counsel
   :after ivy
@@ -362,177 +518,3 @@ _q_ disable                _k_ decrease
 	("j" writeroom-decrease-width :color pink)
 	("k" writeroom-increase-width :color pink)
 	("q" writeroom-mode :color blue)))
-
-(use-package web-mode
-  :mode ("\\.html?\\'" "\\.css\\'" "\\.tsx\\'"))
-
-(use-package company
-  :diminish 'company-mode
-  :custom
-  (company-dabbrev-downcase nil)
-  (company-idle-delay 0.1)
-  :config
-  (global-company-mode))
-
-(use-package magit
-  :config
-  ; Disables the automatic diff show-off of the changes about to commit.
-  (remove-hook 'server-switch-hook 'magit-commit-diff)
-  (add-to-list 'magit-no-confirm 'stage-all-changes)
-  (setq vc-handled-backends '(git))
-  (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
-  (evil-set-initial-state 'magit-log-edit-mode 'insert)
-  (add-hook 'git-commit-mode-hook 'evil-insert-state)
-  :general
-  ("C-x g" 'magit-status))
-
-;; Elm
-
-(use-package elm-mode
-  :mode ("\\.elm$" . elm-mode)
-  :after company
-  :config
-  (add-to-list 'company-backends 'company-elm))
-
-;; Python
-
-(defun my/python-mode ()
-  (setq tab-width 4)
-  (setq indent-tabs-mode t)
-  (setq indent-line-function 'insert-tab))
-
-(use-package anaconda-mode
-  :mode ("\\.py$" . python-mode)
-  :diminish anaconda-mode
-  :config
-  (add-hook 'python-mode-hook 'anaconda-mode)
-  (add-hook 'python-mode-hook 'my/python-mode))
-
-(use-package company-anaconda
-  :after (company anaconda-mode)
-  :config
-  (add-to-list 'company-backends 'company-anaconda))
-
-;; Lua
-
-(defun my/lua-mode ()
-  (setq tab-width 4)
-  (setq indent-tabs-mode t)
-  (setq indent-line-function 'insert-tab)
-  (setq lua-indent-level 4))
-
-(use-package lua-mode
-  :mode ("\\.lua$" . lua-mode)
-  :config
-  (add-hook 'lua-mode-hook 'my/lua-mode))
-
-(use-package exec-path-from-shell
-  :if (memq window-system '(mac ns x))
-  :config
-  (exec-path-from-shell-initialize))
-
-(use-package markdown-mode
-  :mode ("\\.md\\'"))
-
-(use-package rg
-  :config
-  (setq rg-hide-command t)
-  (setq rg-group-result t)
-  (rg-define-toggle "--context 3" (kbd "C-c c")))
-
-(use-package typo
-  :diminish 'typo-mode
-  :init
-  (add-hook 'org-mode-hook 'typo-mode))
-
-(use-package yaml-mode
-  :mode ("\\.yml$". yaml-mode))
-
-;; Rust
-
-(defun my/rust-settings ()
-  "Bunch of default settings valid for rust-mode"
-  (interactive)
-  (setq tab-width 4)
-  (setq c-basic-offset 4)
-  (setq indent-tabs-mode t)
-  (modify-syntax-entry ?_ "w"))
-
-(use-package rust-mode
-  :mode ("\\.rs$" . rust-mode)
-  :config
-  (add-hook 'rust-mode-hook 'my/rust-settings))
-
-;; Haskell
-
-(defun my/haskell-settings ()
-  "Bunch of default settings valid for haskell-mode"
-  (interactive)
-  (setq tab-width 2)
-  (setq c-basic-offset 2)
-  (setq indent-tabs-mode nil))
-
-(use-package haskell-mode
-  :mode ("\\.hs\\'" "\\.lhs\\'" "\\.hsc\\'" "\\.cpphs\\'" "\\.c2hs\\'")
-  :config
-  (setq haskell-compile-cabal-build-command "stack build")
-  (add-hook 'haskell-mode 'my/haskell-settings))
-
-(use-package dockerfile-mode
-  :mode ("Dockerfile\\'"))
-
-(use-package typescript-mode
-  :mode ("\\.ts\\'"))
-
-(use-package swift-mode
-  :mode ("\\.swift\\'"))
-
-(use-package csharp-mode
-  :mode ("\\.cs\\'"))
-
-(use-package kotlin-mode
-  :mode ("\\.kt\\'"))
-
-(use-package solidity-mode
-  :mode ("\\.sol\\'")
-  :config
-  (setq solidity-comment-style 'slash))
-
-(use-package php-mode
-  :mode ("\\.php\\'"))
-
-(use-package company-solidity
-  :after (company solidity-mode)
-  :config
-  (add-to-list 'company-backends 'company-solidity))
-
-(use-package eglot)
-
-;;; Bindings
-
-(defconst my/leader "SPC")
-(general-create-definer my/leader-def :prefix my/leader)
-
-(general-define-key
-  :states 'normal
-  "C-h"     'windmove-left
-  "C-j"     'windmove-down
-  "C-k"     'windmove-up
-  "C-l"     'windmove-right)
-
-(general-define-key
- :states 'insert
- "C-v" 'evil-paste-before)
-
-(my/leader-def
-  :states 'normal
-  "d"   'my/kill-current-buffer
-  "i"   'imenu
-  "e i" 'my/find-user-init-file
-  "r"   'rg
-  "w"   'writeroom-mode
-  "p"   'counsel-fzf)
-
-(my/leader-def
-  :states 'visual
-  "c" 'comment-or-uncomment-region)
