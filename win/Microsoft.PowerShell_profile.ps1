06eaf771c934b92c593d194e6c1eafabece6ca1f
Denys Mentiei <endden@gmail.com>
2023-05-28 13:48:49 +0200

    powershell: Git aliases added.

--- win/Microsoft.PowerShell_profile.ps1
+++ win/Microsoft.PowerShell_profile.ps1
@@ -24,3 +24,41 @@ function limit-HomeDirectory($Path) {
 Invoke-Expression (&starship init powershell)
 
 ## Aliases
+
+# Preventing conflict with builtins.
+if ($PSVersionTable.PSVersion.Major -le 5) {
+	function Remove-Alias ([string] $AliasName) {
+		while (Test-Path Alias:$AliasName) {
+			Remove-Item Alias:$AliasName -Force 2> $null
+		}
+	}
+}
+Remove-Alias gc -Force -ErrorAction SilentlyContinue
+Remove-Alias gp -Force -ErrorAction SilentlyContinue
+Remove-Alias gl -Force -ErrorAction SilentlyContinue
+
+function g { git $args }
+
+function gst { git status $args }
+
+function gco { git checkout $args }
+function gcb { git checkout -b $args }
+
+function gd { git diff $args }
+function gdc { git diff --cached $args }
+
+function ga { git add $args }
+function gaa { git add --all $args }
+
+function gb { git branch -vv $args }
+function gba { git branch -vv -a $args }
+
+function gc { git commit -v $args }
+function gc! { git commit -v --amend $args }
+function gcn! { git commit -v --amend --no-edit $args }
+
+function gp { git push $args}
+function gp! { git push --force-with-lease $args }
+
+function gf { git fetch --all --prune $args }
+function gl { git pull -ff $args }
