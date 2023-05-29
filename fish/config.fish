94e3607aa577ab5d1b0ab319033f48e45d212a24
Denys Mentiei <endden@gmail.com>
2023-05-29 13:54:22 +0200

    fish: Set EDITOR/VISUAL.

--- fish/config.fish
+++ fish/config.fish
@@ -18,6 +18,8 @@ load_keys
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
+	set -Ux EDITOR emacsclient -c
+	set -Ux VISUAL emacsclient -c
 end
 
 # Trying out the starship promt.
