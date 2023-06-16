daee82a803ea630203c437efa556866cd6123a7f
Denys Mentiei <endden@gmail.com>
2023-06-16 20:28:57 +0200

    psh: Aliases for cargo.

--- win/Microsoft.PowerShell_profile.ps1
+++ win/Microsoft.PowerShell_profile.ps1
@@ -33,6 +33,8 @@ Remove-Alias gc -Force -ErrorAction SilentlyContinue
 Remove-Alias gp -Force -ErrorAction SilentlyContinue
 Remove-Alias gl -Force -ErrorAction SilentlyContinue
 
+### Git
+
 function g { git $args }
 
 function gst { git status $args }
@@ -51,13 +53,26 @@ function gba { git branch -vv -a $args }
 
 function gc { git commit -v $args }
 function gc! { git commit -v --amend $args }
-function gcn! { git commit -v --amend --no-edit $args }
+function gcan! { git commit -v --amend --no-edit $args }
 
 function gp { git push $args}
 function gp! { git push --force-with-lease $args }
 
 function gf { git fetch --all --prune $args }
 function gl { git pull -ff $args }
+
+### Cargo
+
+function c { cargo $args }
+function ccl { cargo clean $args }
+function cck { cargo check $args }
+function cb { cargo build $args }
+function cr { cargo run $args }
+function cwc { cargo watch -x 'check' $args }
+function cwt { cargo watch -x 'test' $args }
+function cwb { cargo watch -x 'build' $args }
+
+### Etc.
 
 Remove-Alias ls -Force -ErrorAction SilentlyContinue
 function la { ls -lah --color=auto $args }
